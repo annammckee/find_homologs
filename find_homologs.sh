bada59f322cdb17f8a06a55aa1a5bb391dcb20e8
snflk620 <anna.m.mckee@gmail.com>
2023-09-09 00:29:15 -0400

    changed blast to tblastn

--- find_homologs.sh
+++ find_homologs.sh
@@ -8,15 +8,11 @@ query="$1"
 subject="$2"
 output="$3"
 
-##perform blast on the query file against the subject file where the length of the match is the same length as the 
-##subject file (we know this is 28 bp per the assignment description) and the sequences are 100% identical
-## output the header of the matching subject sequence (qseqid)
-blastn -query "$query" -subject "$subject" -task blastn-short -perc_identity 100 -word_size 28 -outfmt '6 qseqid' -out "$output" 
 
-##Count the number of lines in the output file to determine how many perfect matches there are
-## using wc -1 < "$output" will print the number of lines without also printing the filename
-##in contrast wc -1 "$output" would also print the file name
-matching_sequence_count=$(wc -l < "$output")
+##changed the blastn command to tblastn to screen a protein sequence (query) against nucleotide seqs
+## want at least 30% identity
+## only want results that have at least a 90% match length
+tblastn -query "$query" -subject "$subject" -perc_identity 30 -outfmt '6' -out "$output" | awk '$4/$13' ;> 0.9 "$output" > "$output" | wc -l "$output"
 
-##print the number of matching sequences to stdout
-echo "Number of perfect matches: $matching_sequence_count"
+
+echo "Number of matches: $output"
